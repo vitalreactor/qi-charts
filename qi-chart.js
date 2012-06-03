a9a1d0dd2e8ef7a8e4d1dc4e7b63e5182c0936f9
Ian Eslick <eslick@alum.mit.edu>
2012-06-02 17:00:59 -0700

    Add support for rendering control charts, requires external generation of hints, annotations, etc

--- qi-chart.js
+++ qi-chart.js
@@ -30,32 +30,37 @@ var QIchart = {
 	},
 
     renderTimeAxis: function (chart,xScale,cfg) {
+  		var x_fmt = xScale.tickFormat(30); // d3.time.format("%m/%e"); // 
+		var dsel = function (d) { return "translate( " + xScale(d) + " 0)"; }
+		var top = (cfg.render.xaxis === 'top')
+		var offset = (top ? 10 : cfg.h)
+		var textoffset = (top ? 0 : cfg.h)
+		var texttrans = (top ? -5 : 5)
 
-	var x_fmt = xScale.tickFormat(30); // d3.time.format("%m/%e"); // 
-	var dsel = function (d) { return "translate( " + xScale(d) + " 0)"; }
-
-	/* Grouping */
-	var ticks = chart.selectAll('.tick')
+		/* Grouping */
+		var ticks = chart.selectAll('.tick')
 			.data(xScale.ticks(30))
 			.enter().append('svg:g')
 			.attr('transform', dsel)
 			.attr('class', cfg.defaults.timeaxis);
 	    
-	/* Tick Marks  */
-	ticks.append("svg:line")
-			.attr("x1", 0)
-			.attr("x2", 0)
-			.attr("y1", cfg.h-10)
-  			.attr("y2", cfg.h);
+		/* Tick Marks  */
+        if ( !top ) {
+  			ticks.append("svg:line")
+				.attr("x1", 0)
+				.attr("x2", 0)
+				.attr("y1", offset-10)
+  				.attr("y2", offset);
+		}
 
-	/* Labels */
-	ticks.append("svg:text")
+		/* Labels */
+		ticks.append("svg:text")
 			.text(x_fmt)
 			.style("fill", "grey")
 			.attr("font-size", "10px")
-			.attr("text-anchor", "end")
-			.attr("transform", "translate(5 5) rotate(270 0 " + cfg.h + ")")
-			.attr("y", cfg.h);
+			.attr("text-anchor", (top ? "begin" : "end"))
+			.attr("transform", "translate(5 5) rotate(270 0 " + textoffset + ")")
+			.attr("y", textoffset);
     },
 
     renderValueAxis: function(chart,yScale,offset,cfg) {
@@ -101,11 +106,37 @@ var QIchart = {
 	    return _.reduce(data, function (a,b) { return a + b; }) / _.size(data);
     },
 
+    minmax: function(data) {
+		return _.reduce(data, function (mm,val) { 
+			mm.min = (mm.min !== null) ? Math.min(mm.min, val) : val
+			mm.max = (mm.max !== null) ? Math.max(mm.max, val) : val
+			return mm
+		}, {min: null, max: null});
+	},
+
+    range: function(data, scale) {
+		var mm = this.minmax(data)
+        var upper = scale(mm.max);
+        var lower = scale(mm.min);
+        var range = Math.abs(upper - lower); 
+		return range;
+	},
+
+    centroid: function(data) {
+		var mm = this.minmax(data);
+		var hd = (mm.max - mm.min);
+        if ( hd == 0 ) {
+			return mm.min;
+		} else {
+			return mm.min + hd / 2;
+		}
+		return center;
+	},
+
     xScale: function(dates,cfg) {
 		var min;
 		var max;
-		console.log([cfg.start, cfg.end]);
-		if ( cfg.start == null && cfg.end == null ) {
+		if ( cfg.start == null && cfg.end == null && dates.length > 0 ) {
 			min = d3.min(dates);
 			max = d3.max(dates);
 /*
@@ -118,15 +149,19 @@ var QIchart = {
 			min = cfg.start;
 			max = cfg.end;
 		}
-	    return d3.time.scale()
-	        .domain([min, max])
-	        .range([0,cfg.w]);
+		if (_.isNumber(min) && _.isNumber(max)) {
+  			return d3.time.scale()
+	          .domain([min, max])
+	          .range([0,cfg.w]);
+		} else {
+            return null;
+		}
     },
     
     yScale: function(data,cfg) {
 		var min;
 		var max;
-        if ( cfg.dataMin == null ) {
+        if ( cfg.dataMin == null && data.length > 0 ) {
 			min = d3.min(data);
 			max = d3.max(data);
 			var range = max - min;
@@ -138,10 +173,14 @@ var QIchart = {
 			max = cfg.dataMax;
 		}
 
-        return d3.scale.linear()
-            .domain([min, max])
-		    .nice()
-            .range([cfg.h,0]);
+		if (_.isNumber(min) && _.isNumber(max)) {
+          return d3.scale.linear()
+              .domain([min, max])
+  		      .nice()
+              .range([cfg.h,0]);
+		} else {
+		  return null;
+		}
     },
 
     isWeekend: function (ts) {
@@ -154,7 +193,31 @@ var QIchart = {
 		}
 	},
 
-    renderSeries: function (chart,series,xscale,yscale,cfg,mean) {
+
+    dashedLine: function (chart, yscaled, cfg) {
+        chart.append("line")
+			.attr('stroke', 'grey')
+			.attr('stroke-width', '1px')
+            .attr('stroke-dasharray', '5,5')
+            .attr('stroke-opacity', '0.5')
+			.attr('x1', 0)
+			.attr('y1', yscaled)
+            .attr('x2', cfg.w)
+			.attr('y2', yscaled)
+	},
+
+    solidLine: function (chart, yscaled, cfg) {
+        chart.append("line")
+			.attr('stroke', 'grey')
+			.attr('stroke-width', '1px')
+            .attr('stroke-opacity', '0.5')
+			.attr('x1', 0)
+			.attr('y1', yscaled)
+            .attr('x2', cfg.w)
+			.attr('y2', yscaled)
+	},
+
+    renderData: function (chart,series,xscale,yscale,cfg,mean) {
         var xsel = function (d,i) { return xscale(d.ts); }
         var ysel = function (d,i) { return yscale(d.v); }
         var titlesel = function (d,i) { 
@@ -211,15 +274,7 @@ var QIchart = {
 		if (mean) {
     	    var mean = this.mean(_.pluck(series, 'v'));
 
-			chart.append("line")
-				.attr("stroke-width", "1px")
-				.attr("stroke", "grey")
-				.attr("fill", "grey")
-				.attr("stroke-opacity", "0.5")
-				.attr("x1", 0)
-				.attr("y1", yscale(mean))
-				.attr("x2", cfg.w)
-				.attr("y2", yscale(mean));
+            this.solidLine(chart, yscale(mean), cfg);
 
 			if ( cfg.render.meanOffsets ) {
 				chart.selectAll("line")
@@ -241,7 +296,7 @@ var QIchart = {
 		var width = x2 - x1;
 
 		chart.append('rect')
-			.style("fill", "red")
+			.style("fill", region.color || "red")
 			.style("fill-opacity", 0.1)
 			.attr("x", x1)
 			.attr("y", 0)
@@ -259,6 +314,36 @@ var QIchart = {
 		}
 
     },
+
+    renderSignificantEvents: function (chart, xscale, yscale, sigs) {
+		_.each(sigs, function (sig) { 
+            if ( sig.type === "point" ) {
+ 				var x = xscale(sig.ts);
+				var y = yscale(sig.v);
+				var radius = 10;        
+				chart.append("circle")
+  					.style("stroke", "red")
+					.style("fill", "none")
+					.attr("cx", x)
+					.attr("cy", y)
+					.attr("r", radius)
+			} else if ( sig.type === "sequence" ) {
+				var ts = _.pluck( sig.points, 'ts' )
+				var vs = _.pluck ( sig.points, 'v' )
+				var x = xscale( this.centroid( ts ) );
+                var rx = this.range( ts, xscale ) * 0.75;
+                var y = yscale( this.centroid ( vs ) );
+                var ry = this.range( vs, yscale ) * 0.75;
+				chart.append("ellipse")
+				    .style("stroke", "red")
+				    .style("fill", "none")
+				    .attr("cx", x)
+				    .attr("cy", y)
+				    .attr("rx", rx)
+				    .attr("ry", ry)
+			}
+		}, this);
+	},
   
     boundingBox: function (chart, cfg) {
 		chart.append('rect')
@@ -270,64 +355,43 @@ var QIchart = {
 			.attr("height", cfg.h);
     },
 
-    runChart: function (cid,data,cfg) {
-		var extra;
-        if ( cfg.render.xaxis ) {
-			extra = cfg.h + cfg.margin*3;
-		} else {
-			extra = cfg.h + cfg.margin;
-		}
+    renderSeries: function (chart, series, xscale, yscale, cfg) {
+        data = series.data;
+		cfg.start = series.start;
+		cfg.end = series.end;
+		cfg.dataMin = series.dataMin;
+		cfg.dataMax = series.dataMax;
 
-		console.log(extra);
+		/* Configure Chart Boundaries */
+		if (cfg.render.xaxis && xscale) {
+			this.renderTimeAxis(chart,xscale,cfg);
+		}
+		if (cfg.render.yaxis && yscale) {
+  			this.renderValueAxis(chart,yscale,0,cfg);
+		}
+		if (data.length > 0 && xscale && yscale) {
+  			this.renderData(chart,data,xscale,yscale,cfg,true);
+		}
+	},
+
+    isValidSeries: function (series) {
+        return (series.data != null) && (series.data[0] != null) && (_.isNumber(series.data[0].v));
+	},
+
+    renderChart: function (cid,dataset,cfg) {
+		var extra = cfg.h + ( cfg.render.xaxis ? cfg.margin*3 : cfg.margin )
+		var topmargin = ( cfg.render.xaxis === 'top' ? cfg.margin*2 : cfg.margin )
+
 		var chart = d3.selectAll(cid).append("svg")
-			.attr("width", cfg.w + cfg.margin*2.5)
+			.attr("width", cfg.w + cfg.margin*3)
 			.attr("height", extra)
 			.append("g")
-			.attr("transform", "translate(" + cfg.margin * 2.5 + "," + cfg.margin + ")");
-
-        var series = data.series;
-		if ( series.length > 0 ) {
-			cfg.start = data.start;
-			cfg.end = data.end;
-			cfg.dataMin = data.dataMin;
-			cfg.dataMax = data.dataMax;
-
-			var ds = _.pluck(series, 'ts')
-			var vs = _.pluck(series, 'v')
-
-			var xscale = this.xScale(ds, cfg);
-			var yscale = this.yScale(vs, cfg);
-
-/*		this.boundingBox(chart, cfg); */
-			if (cfg.render.xaxis) {
-				console.log('render xaxis')
-				this.renderTimeAxis(chart,xscale,cfg);
-			}
-			if (cfg.render.yaxis) {
-				console.log('render yaxis')
-  				this.renderValueAxis(chart,yscale,0,cfg);
-			}
-			if (series.length > 0) {
-				console.log('render series')
-  				this.renderSeries(chart,series,xscale,yscale,cfg,true);
-			}
-		} else {
-			console.log('render no data')
-			chart.append("svg:text")
-			    .attr("x", cfg.w/2)
-			    .attr("y", cfg.h/2)
-				.attr("text-anchor", "middle")
-			    .style("stroke", "none")
-			    .style("fill", "black")
-			    .text("No Data Found")
-		}
+			.attr("transform", "translate(" + cfg.margin * 2.5 + "," + topmargin + ")");
 
 		/* Title */
 		if ( cfg.title ) {
-			console.log('render title: ' + cfg.title)
 			var midX = -cfg.margin*2;
 			var midY = cfg.h / 2;
-
 			chart.append("svg:text")
 				.attr("x", midX)
 				.attr("y", midY)
@@ -339,19 +403,75 @@ var QIchart = {
 				.text(cfg.title); 
 		}
 
-		/* Render regions if provided */
-        if (data.regions) {
-            _this = this;
-			_.each(data.regions, function (region) {
-   				_this.renderRegionHighlight(chart, xscale, region, cfg);
-			});
-		}
-    },
+        var series = dataset.series;
+        var primary = series[0]
+		var pdata = primary.data
+		cfg.start = primary.start;
+		cfg.end = primary.end;
+		cfg.dataMin = primary.dataMin;
+		cfg.dataMax = primary.dataMax;
+		var ds = _.pluck(pdata, 'ts')
+		var vs = _.pluck(pdata, 'v')
+		var xscale = this.xScale(ds, cfg);
+		var yscale = this.yScale(vs, cfg);
 
-    // CONTROL CHART 
-    controlChart: function (cid,d,cfg) {
-  		alert( 'not implemented' );
-    }
+        /* Control Lines */
+        if ( primary.ctrl && primary.ctrl.ucl ) {
+			console.log('render ucl')
+            this.dashedLine(chart, yscale(primary.ctrl.ucl), cfg)
+		}
+
+        if ( primary.ctrl && primary.ctrl.lcl ) {
+			console.log('render lcl')
+            this.dashedLine(chart, yscale(primary.ctrl.lcl), cfg)
+		}
+
+		/* Render regions if provided */
+        if (primary.regions) {
+			_.each(primary.regions, function (region) {
+                if (region.type === 'area')
+     			    this.renderRegionHighlight(chart, xscale, region, cfg);
+                else if (region.type === 'label')
+                    this.renderLabelHighlight(chart, xscale, region, cfg);
+			}, this);
+		}
+
+		/* Main data series, only plot axes of first series */
+		if (series.length > 0) {
+            if (!this.isValidSeries(primary)) {
+   			    primary.data = [];
+		    }
+			this.renderSeries(chart, primary, xscale, yscale, _.extend({}, cfg));
+			_.each(_.rest(series), function (s) {
+                _cfg = _.extend({},cfg);
+                _cfg.xaxis = false
+                _cfg.yaxis = false
+				this.renderSeries(chart,s,xscale,yscale,cfg);
+			}, this);
+		}
+		if ( primary.data.length === 0 ) {
+			chart.append("svg:text")
+			    .attr("x", cfg.w/2)
+			    .attr("y", cfg.h/2)
+				.attr("text-anchor", "middle")
+			    .style("stroke", "none")
+			    .style("fill", "black")
+			    .text("No Data Found")
+            return null;
+		}
+
+        /* Significant events */
+        if ( primary.significance !== undefined && primary.significance.length > 0 ) {
+			this.renderSignificantEvents(chart, xscale, yscale, primary.significance);
+		}
+		
+        /* Render callouts if provided */
+        if ( primary.callouts !== undefined && primary.callouts.length > 0 ) {
+            _.each(data.callouts, function (callout) {
+                _this.renderCallout(chart, xscale, yscale, callout, cfg);
+			}, this);
+		}
+	},
 };
     return QIchart;
 });
